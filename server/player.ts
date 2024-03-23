165409f2886fac34b0a3b7a2cba52d05381d3e08
Timm TheBlokker <theblokker.dev@gmail.com>
2024-03-23 17:23:20 +0100

    adding player functions

--- server/player.ts
+++ server/player.ts
@@ -511,13 +511,213 @@ function SavePlayerData(player: PlayerData) {
     }
 }
 
+/**
+ * Sets the job for the given player.
+ *
+ * @param player - The player to set the job for.
+ * @param jobname - The name of the job.
+ * @param joblabel - The display label for the job. 
+ * @param jobpayment - The payment amount for the job.
+ * @param jobtype - The type of job.
+ * @param jobonduty - Whether the job is on duty.
+ * @param jobisboss - Whether the player is the boss of the job.
+ * @param jobgrade - The job grade object containing name and level.
+ */
+function setJob(player: alt.Player, jobname: string, joblabel: string, jobpayment: number, jobtype: string, jobonduty: boolean, jobisboss: boolean, jobgrade: { name: string, level: number, extrasallery: Array<string> }) {
+    jobname.toLowerCase();
+    player.setMeta('job:name', jobname);
+    player.setMeta('job:label', joblabel);
+    player.setMeta('job:payment', jobpayment);
+    player.setMeta('job:type', jobtype);
+    player.setMeta('job:onduty', jobonduty);
+    player.setMeta('job:isboss', jobisboss);
+    player.setMeta('job:gradename', jobgrade.name);
+    player.setMeta('job:gradelevel', jobgrade.level);
+}
 
 
+/**
+ * Resets the job metadata for the given player. 
+ *
+ * Sets all job metadata fields to default values.
+ * @param player - The player to reset the job for.
+ */
+function ResetJob(player: alt.Player) {
+    player.setMeta('job:name', '');
+    player.setMeta('job:label', '');
+    player.setMeta('job:payment', 0);
+    player.setMeta('job:type', '');
+    player.setMeta('job:onduty', false);
+    player.setMeta('job:isboss', false);
+    player.setMeta('job:gradename', '');
+    player.setMeta('job:gradelevel', 0);
+}
+
+/**
+ * Sets the gang for the given player.
+ * 
+ * @param player - The player to set the gang for
+ * @param gangname - The name of the gang
+ * @param ganglabel - The display label for the gang
+ * @param gangisboss - Whether the player is the boss of the gang
+ * @param ganggrade - The gang grade object containing name and level
+ */
+function SetGang(player: alt.Player, gangname: string, ganglabel: string, gangisboss: boolean, ganggrade: { name: string, level: string }) {
+    gangname.toLowerCase();
+    player.setMeta('gang:name', gangname);
+    player.setMeta('gang:label', ganglabel);
+    player.setMeta('gang:isboss', gangisboss);
+    player.setMeta('gang:gradename', ganggrade.name);
+    player.setMeta('gang:gradelevel', ganggrade.level);
+}
+
+/**
+ * Resets the gang metadata for the given player.
+ * 
+ * Sets all gang metadata fields to default values.
+ * @param player - The player to reset the gang for.
+ */
+function ResetGang(player: alt.Player) {
+    player.setMeta('gang:name', '');
+    player.setMeta('gang:label', '');
+    player.setMeta('gang:isboss', false);
+    player.setMeta('gang:gradename', '');
+    player.setMeta('gang:gradelevel', 0);
+}
+
+/**
+ * Adds the specified amount of money to the given player's balance.
+ * 
+ * @param player - The player to add money to.
+ * @param moneytype - The type of money to add ('cash', 'bank', 'crypto'). 
+ * @param amount - The amount of money to add.
+ * @param reason - The reason for adding the money.
+ */
+function AddMoney(player: alt.Player, moneytype: ('cash' | 'bank' | 'crypto'), amount: number, reason: string) {
+    if (moneytype == 'cash') {
+        let cash = player.getMeta<number>('money:cash');
+        if (cash !== undefined) {
+            cash += amount;
+        } else {
+            return;
+        }
+        player.setMeta('money:cash', cash);
+        alt.log(`[Money] Added ${amount} cash to ${player.name} (${reason}). New balance is ${cash}.`);
+    } else if (moneytype == 'bank') {
+        let bank = player.getMeta<number>('money:bank');
+        if (bank !== undefined) {
+            bank += amount;
+        } else {
+            return;
+        }
+        player.setMeta('money:bank', bank);
+        alt.log(`[Money] Added ${amount} bank to ${player.name} (${reason}). New balance is ${bank}.`);
+    } else if (moneytype == 'crypto') {
+        let crypto = player.getMeta<number>('money:crypto');
+        if (crypto !== undefined) {
+            crypto += amount;
+        } else {
+            return;
+        }
+        player.setMeta('money:crypto', crypto);
+        alt.log(`[Money] Added ${amount} crypto to ${player.name} (${reason}). New balance is ${crypto}.`);
+    } else {
+        return;
+    }
+    alt.emit('hud:client:UpdateMoney', player, moneytype, amount, false);
+    alt.emit('trcore:client:UpdateMoney', player, moneytype, amount, "add", reason);
+
+}
+
+/**
+ * Removes the specified amount of money from the given player's balance.
+ * 
+ * @param player - The player to remove money from.
+ * @param moneytype - The type of money to remove ('cash', 'bank', 'crypto').
+ * @param amount - The amount of money to remove. 
+ * @param reason - The reason for removing the money.
+ */
+function RemoveMoney(player: alt.Player, moneytype: ('cash' | 'bank' | 'crypto'), amount: number, reason: string) {
+    if (moneytype == 'cash') {
+        let cash = player.getMeta<number>('money:cash');
+        if (cash !== undefined) {
+            cash -= amount;
+        } else {
+            return;
+        }
+        player.setMeta('money:cash', cash);
+        alt.log(`[Money] Remove ${amount} cash to ${player.name} (${reason}). New balance is ${cash}.`);
+    } else if (moneytype == 'bank') {
+        let bank = player.getMeta<number>('money:bank');
+        if (bank !== undefined) {
+            bank -= amount;
+        } else {
+            return;
+        }
+        player.setMeta('money:bank', bank);
+        alt.log(`[Money] Remove ${amount} bank to ${player.name} (${reason}). New balance is ${bank}.`);
+    } else if (moneytype == 'crypto') {
+        let crypto = player.getMeta<number>('money:crypto');
+        if (crypto !== undefined) {
+            crypto -= amount;
+        } else {
+            return;
+        }
+        player.setMeta('money:crypto', crypto);
+        alt.log(`[Money] Remove ${amount} crypto to ${player.name} (${reason}). New balance is ${crypto}.`);
+    } else {
+        return;
+    }
+    alt.emitClient(player, 'hud:client:UpdateMoney', moneytype, amount, true);
+    alt.emitClient(player, 'trcore:client:UpdateMoney', moneytype, amount, "remove", reason);
+}
+
+
+/**
+ * Gets the specified type of money for the given player.
+ * 
+ * @param player - The player to get the money for. 
+ * @param moneytype - The type of money to get ('cash', 'bank', 'crypto').
+ * @returns The amount of the specified moneytype for the player, or 0 if not defined.
+ */
+function GetMoney(player: alt.Player, moneytype: ('cash' | 'bank' | 'crypto')) {
+    if (moneytype == 'cash') {
+        let cash = player.getMeta<number>('money:cash');
+        if (cash !== undefined) {
+            return cash;
+        } else {
+            return 0;
+        }
+    } else if (moneytype == 'bank') {
+        let bank = player.getMeta<number>('money:bank');
+        if (bank !== undefined) {
+            return bank;
+        } else {
+            return 0;
+        }
+    } else if (moneytype == 'crypto') {
+        let crypto = player.getMeta<number>('money:crypto');
+        if (crypto !== undefined) {
+            return crypto;
+        } else {
+            return 0;
+        }
+    } else {
+        return 0;
+    }
+}
 
 /**
  * Exports the PlayerData class.
  */
 export {
     PlayerData,
-    SavePlayerData
+    SavePlayerData,
+    setJob,
+    ResetJob,
+    SetGang,
+    ResetGang,
+    AddMoney,
+    RemoveMoney,
+    GetMoney
 }
